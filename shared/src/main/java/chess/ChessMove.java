5cc64fa9b1f17a0c1ee64be8d3d6a23c051d0d5b
Kiara Chambers <KiaraChambers3@gmail.com>
2026-01-20 15:59:55 -0700

    object overrides

--- shared/src/main/java/chess/ChessMove.java
+++ shared/src/main/java/chess/ChessMove.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.Objects;
+
 /**
  * Represents moving a chess piece on a chessboard
  * <p>
@@ -43,7 +45,21 @@ public class ChessMove {
         return promotionPiece;
     }
 
-    public String toString(){
-        return String.format("%s%s",startPosition,endPosition);
+    public String toString() {
+        return String.format("%s%s", startPosition, endPosition);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        ChessMove chessMove = (ChessMove) o;
+        return Objects.equals(startPosition, chessMove.startPosition) && Objects.equals(endPosition, chessMove.endPosition) && promotionPiece == chessMove.promotionPiece;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(startPosition, endPosition, promotionPiece);
     }
 }
