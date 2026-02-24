9072178688af23ea9dcfabc944476902a42ed9ec
Kiara Chambers <KiaraChambers3@gmail.com>
2026-02-23 23:18:48 -0700

    starting authService

--- server/src/main/java/service/AuthService.java
+++ server/src/main/java/service/AuthService.java
@@ -1,4 +1,23 @@
 package service;
 
+import dataaccess.MemoryAuthDAO;
+import dataaccess.MemoryUserDAO;
+import model.UserData;
+
 public class AuthService {
+    MemoryUserDAO userDAO;
+    MemoryAuthDAO authDAO;
+
+    public AuthService(MemoryUserDAO userDAO, MemoryAuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public String register(String username, String password, String email){
+
+    }
+    public String login(){}
+    public void logout(){}
+    public void clear(){}
+    public UserData getUserFromToken(){}
 }
