93ed994bcda9d894f2eba61af9c62e2bda84d11a
Kiara Chambers <KiaraChambers3@gmail.com>
2026-02-23 22:23:14 -0700

    services and tests

---
+++ shared/src/main/java/service/UserService.java
@@ -0,0 +1,17 @@
+package service;
+
+public class UserService {
+    public RegisterResult register(RegisterRequest registerRequest) {}
+    public LoginResult login(LoginRequest loginRequest) {}
+    public void logout(LogoutRequest logoutRequest) {}
+}
+record LoginRequest(
+        String username,
+        String password){
+}
+record UserData(
+        String username,
+        String password,
+        String email){
+}
+record LoginResult(String username, String authToken) {}
