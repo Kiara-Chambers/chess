6a887295c3c0d56856792ed56ecf7d8a6b64d2eb
Kiara Chambers <KiaraChambers3@gmail.com>
2026-02-23 23:14:43 -0700

    apparently those exceptions are not supposed to go there, haha

--- server/src/main/java/dataaccess/MemoryAuthDAO.java
+++ server/src/main/java/dataaccess/MemoryAuthDAO.java
@@ -9,21 +9,21 @@ import java.util.UUID;
 public class MemoryAuthDAO {
     Map<String, UserData> authTokens = new HashMap<>();
 
-    public void clear() throws DataAccessException {
+    public void clear() {
         authTokens.clear();
     }
 
-    public String createAuth(UserData user) throws DataAccessException {
+    public String createAuth(UserData user) {
         String token = UUID.randomUUID().toString();
         authTokens.put(token, user);
         return token;
     }
 
-    public UserData getAuth(String token) throws DataAccessException {
+    public UserData getAuth(String token) {
         return authTokens.get(token);
     }
 
-    public void deleteAuth(String token) throws DataAccessException {
+    public void deleteAuth(String token) {
         authTokens.remove(token);
     }
 }
