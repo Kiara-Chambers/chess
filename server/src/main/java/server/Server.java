2ac596decda4222f47cf28e08be3fd068365e0cf
Kiara Chambers <KiaraChambers3@gmail.com>
2026-02-24 20:06:14 -0700

    summoned the handlers

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -13,32 +13,55 @@ import service.UserService;
 public class Server {
 
     private final Javalin javalin;
-
+    UserService userService;
+    GameService gameService;
+    ClearService clearService;
 
     public Server() {
         var userDAO = new MemoryUserDAO();
         var gameDAO = new MemoryGameDAO();
         var authDAO = new MemoryAuthDAO();
 
-        UserService userService= new UserService(userDAO,authDAO);
-        GameService gameService = new GameService(gameDAO, authDAO);
-        ClearService clearService = new ClearService(userDAO,authDAO,gameDAO);
+        userService = new UserService(userDAO, authDAO);
+        gameService = new GameService(gameDAO, authDAO);
+        clearService = new ClearService(userDAO, authDAO, gameDAO);
 
 
         javalin = Javalin.create(config -> config.staticFiles.add("web"));
 
         // Register your endpoints and exception handlers here.
-        javalin.post("/user",this::registerHandler);
+        javalin.post("/user", this::registerHandler);
 
-        javalin.post("/session",this::loginHandler);
-        javalin.delete("/session",this::logoutHandler);
+        javalin.post("/session", this::loginHandler);
+        javalin.delete("/session", this::logoutHandler);
 
-        javalin.get("/game",this::listGamesHandler);
-        javalin.put("/game",this::joinGameHandler);
-        javalin.post("/game",this::joinGameHandler);
+        javalin.get("/game", this::listGamesHandler);
+        javalin.put("/game", this::joinGameHandler);
+        javalin.post("/game", this::createGameHandler);
 
-        javalin.delete("/db",this::clearHandler);
+        javalin.delete("/db", this::clearHandler);
 
+    }
+
+    private void createGameHandler(@NotNull Context context) {
+    }
+
+    private void clearHandler(@NotNull Context context) {
+    }
+
+    private void joinGameHandler(@NotNull Context context) {
+    }
+
+    private void listGamesHandler(@NotNull Context context) {
+    }
+
+    private void logoutHandler(@NotNull Context context) {
+    }
+
+    private void loginHandler(@NotNull Context context) {
+    }
+
+    private void registerHandler(@NotNull Context context) {
     }
 
     public int run(int desiredPort) {
