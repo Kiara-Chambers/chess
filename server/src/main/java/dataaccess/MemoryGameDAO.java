77da59b2ac73f5d5a316ce64e9fc4835f780d8e3
Kiara Chambers <KiaraChambers3@gmail.com>
2026-02-25 22:37:11 -0700

    close to what memory game dao should be

--- server/src/main/java/dataaccess/MemoryGameDAO.java
+++ server/src/main/java/dataaccess/MemoryGameDAO.java
@@ -7,28 +7,33 @@ import model.UserData;
 import java.util.*;
 
 public class MemoryGameDAO implements GameDAO {
-    Map<String, ChessGame> games = new HashMap<>();
+    Map<Integer, GameData> games = new HashMap<>();
+    int gameID =1;
 
     public void clear() throws DataAccessException {
         games.clear();
+        gameID = 1;
     }
 
     public int createGame(GameData game) throws DataAccessException
     {
+        int newID = gameID++;
 
-        return 0;
+        GameData newGame = new GameData(newID, game.whiteUsername(), game.blackUsername(),game.gameName(),game.game());
+        games.put(newID,newGame);
+        return newID;
     }
 
     public GameData getGame(int gameID) throws DataAccessException {
-        return null;
+        return games.get(gameID);
     }
 
     public  List<GameData> listGames() throws DataAccessException {
 
-        return List.of();
+        return (List<GameData>) games.values();
     }
 
     public void updateGame(GameData game) throws DataAccessException {
-
+        games.put(game.gameID(),game);
     }
 }
