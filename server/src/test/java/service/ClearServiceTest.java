5aedb6706bc4f3f8420ea3e0381aad52e1a6dcf7
Kiara Chambers <KiaraChambers3@gmail.com>
2026-03-02 19:12:15 -0700

    finished clear test

--- server/src/test/java/service/ClearServiceTest.java
+++ server/src/test/java/service/ClearServiceTest.java
@@ -1,18 +1,34 @@
 package service;
 
+import chess.ChessGame;
 import dataaccess.*;
+import model.GameData;
 import model.UserData;
+import org.eclipse.jetty.server.Authentication;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ClearServiceTest {
-    UserDAO userDAO = new MemoryUserDAO();
-    AuthDAO authDAO = new MemoryAuthDAO();
-    GameDAO gameDAO = new MemoryGameDAO();
 
-    ClearService service = new ClearService(userDAO,authDAO,gameDAO);
+    @Test
+    void clearRemovesEverything() throws DataAccessException {
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
 
-    service.clear();
+        ClearService service = new ClearService(userDAO, authDAO, gameDAO);
 
+        UserData user = new UserData("Kaladin", "pw", "stormblessed@byu.edu");
+        userDAO.createUser(user);
+        authDAO.createAuth(user);
+        gameDAO.createGame(new GameData(1, "Kaladin","Szeth","Storms",new ChessGame()));
 
+        String token  = authDAO.createAuth(user);
 
+        service.clear();
+        assertNull(userDAO.getUser("Kaladin"));
+        assertNull(authDAO.getAuth(token));
+        assertNull(gameDAO.getGame(1));
 
+    }
 }
