1d1a5c9a6ab22341993a08af8514e42b74f357bf
Kiara Chambers <KiaraChambers3@gmail.com>
2026-03-02 20:14:04 -0700

    test register pos

--- server/src/test/java/service/UserServiceTests.java
+++ server/src/test/java/service/UserServiceTests.java
@@ -1,11 +1,23 @@
 package service;
 
+import dataaccess.*;
+import model.UserData;
+import org.eclipse.jetty.server.Authentication;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class UserServiceTests {
     @Test
-    void testRegisterPositive(){
+    void testRegisterPositive() throws DataAccessException {
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        UserService service = new UserService(userDAO,authDAO);
 
+        UserData user = new UserData("Kaladin", "pw", "stormblessed@byu.edu");
+
+        service.register(user);
+
+        assertNotNull(userDAO.getUser("Kaladin"));
     }
     @Test
     void testRegisterNegative(){
