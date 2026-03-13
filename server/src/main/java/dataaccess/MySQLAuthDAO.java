5080f25ce2f0ce33ccb70b2a8d66df2eb727b069
Kiara Chambers <KiaraChambers3@gmail.com>
2026-03-12 22:17:10 -0600

    authdao update

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -11,7 +11,6 @@ import java.util.Map;
 import java.util.UUID;
 
 public class MySQLAuthDAO implements AuthDAO {
-    Map<String, UserData> authTokens = new HashMap<>();
 
     public MySQLAuthDAO() throws DataAccessException {
         createAuthTable();
@@ -23,7 +22,18 @@ public class MySQLAuthDAO implements AuthDAO {
 
     public String createAuth(UserData user) {
         String token = UUID.randomUUID().toString();
-        authTokens.put(token, user);
+        String sql = "INSERT INTO auth (authToken, username) VALUES (?, ?)";
+        try (Connection con = DatabaseManager.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setString(1, token);
+            stmt.setString(2, user.username());
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println("Error creating auth: " + e.getMessage());
+            return null;
+        }
         return token;
     }
 
@@ -51,11 +61,21 @@ public class MySQLAuthDAO implements AuthDAO {
     }
 
     public void deleteAuth(String token) {
-        authTokens.remove(token);
+
+        String sql = "DELETE FROM auth WHERE authToken = ?";
+        try (Connection con = DatabaseManager.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+
+            stmt.setString(1, token);
+            stmt.executeUpdate();
+
+        } catch (Exception e) {
+            System.out.println("Error deleting auth: " + e.getMessage());
+        }
     }
 
     public void createAuthTable() throws DataAccessException {
-        String createTableSQL = "CREATE TABLE IF NOT EXISTS auth(authToken VARCHAR(255), PRIMARY KEY,username VARCHAR(255) NOT NULL ";
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS auth(authToken VARCHAR(255) PRIMARY KEY,username VARCHAR(255) NOT NULL ";
 
         try (Connection con = DatabaseManager.getConnection();
              Statement statement = con.createStatement();
