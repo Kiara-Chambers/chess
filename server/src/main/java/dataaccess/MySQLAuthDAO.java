910122b9344ba05950c100597c5f1afdfae785f6
Kiara Chambers <KiaraChambers3@gmail.com>
2026-03-12 22:03:54 -0600

    create auth table

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -2,12 +2,18 @@ package dataaccess;
 
 import model.UserData;
 
+import java.sql.Connection;
+import java.sql.Statement;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 
 public class MySQLAuthDAO implements AuthDAO{
     Map<String, UserData> authTokens = new HashMap<>();
+
+    public MySQLAuthDAO() throws DataAccessException {
+        createAuthTable();
+    }
 
     public void clear() {
         authTokens.clear();
@@ -25,5 +31,18 @@ public class MySQLAuthDAO implements AuthDAO{
 
     public void deleteAuth(String token) {
         authTokens.remove(token);
+    }
+
+    public void createAuthTable() throws DataAccessException {
+        String createTableSQL = "CREATE TABLE IF NOT EXISTS auth(authToken VARCHAR(255), PRIMARY KEY,username VARCHAR(255) NOT NULL ";
+
+        try (Connection con = DatabaseManager.getConnection();
+             Statement statement = con.createStatement();
+        ) {
+            statement.executeUpdate(createTableSQL);
+
+        } catch (Exception e) {
+            throw new DataAccessException("Error creating auth table:" + e.getMessage());
+        }
     }
 }
