8a2ab00372cee16bbeef551de1ba6cfe1c81a852
Kiara Chambers <KiaraChambers3@gmail.com>
2026-03-12 22:01:29 -0600

    start sql authDAO

--- server/src/main/java/dataaccess/MySQLAuthDAO.java
+++ server/src/main/java/dataaccess/MySQLAuthDAO.java
@@ -1,4 +1,29 @@
 package dataaccess;
 
-public class MySQLAuthDAO {
+import model.UserData;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+public class MySQLAuthDAO implements AuthDAO{
+    Map<String, UserData> authTokens = new HashMap<>();
+
+    public void clear() {
+        authTokens.clear();
+    }
+
+    public String createAuth(UserData user) {
+        String token = UUID.randomUUID().toString();
+        authTokens.put(token, user);
+        return token;
+    }
+
+    public UserData getAuth(String token) {
+        return authTokens.get(token);
+    }
+
+    public void deleteAuth(String token) {
+        authTokens.remove(token);
+    }
 }
