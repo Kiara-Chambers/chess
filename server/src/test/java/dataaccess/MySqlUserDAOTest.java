f2d99a20b2fe656cf5c2399d08ea960c53bda9f0
Kiara Chambers <KiaraChambers3@gmail.com>
2026-03-14 14:50:14 -0600

    create user pos

--- server/src/test/java/dataaccess/MySqlUserDAOTest.java
+++ server/src/test/java/dataaccess/MySqlUserDAOTest.java
@@ -1,4 +1,20 @@
 package dataaccess;
 
+import model.UserData;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class MySqlUserDAOTest {
+    @Test
+    void createUserPositive() throws DataAccessException {
+        MySQLUserDAO userDAO = new MySQLUserDAO();
+        userDAO.clear();
+
+        UserData user = new UserData("Vin","pw","Mistborn@byu.edu");
+        userDAO.createUser(user);
+
+        UserData checkDB = userDAO.getUser("Vin");
+        assertEquals("Vin",checkDB.username());
+    }
 }
