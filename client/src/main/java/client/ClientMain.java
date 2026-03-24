64bba5e38e4881afda86f7e6bd5c12d1381da43b
Kiara Chambers <KiaraChambers3@gmail.com>
2026-03-23 20:33:04 -0600

    start board

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -1,18 +1,21 @@
 package client;
 
 import chess.*;
+import ui.EscapeSequences;
+
+import java.util.Objects;
 
 public class ClientMain {
     public static ServerFacade facade;
     public static void main(String[] args) {
         facade = new ServerFacade(8080);
         System.out.println("♕ 240 Chess Client");
-
+        drawChessBoard("BLACK");
         //menu
     }
 
     public static void menu(){}
-    
+
     public static void register(){}
     public static void login(){}
     public static void logout(){}
@@ -20,7 +23,36 @@ public class ClientMain {
     public static void createGame(){}
     public static void joinGame(){}
 
-    public static void drawChessBoard(boolean perspective){
+    public static void drawChessBoard(String perspective){
+        String[][] board = new String[8][8];
+        for (int i=0;i<8;i++){
+            for(int j =0;j<8;j++){
+                if((i+j)%2==0){
+                    board[i][j] = EscapeSequences.SET_BG_COLOR_BLACK+EscapeSequences.EMPTY;
+                }else{
+                    board[i][j] = EscapeSequences.SET_BG_COLOR_WHITE+EscapeSequences.EMPTY;
+                }
+            }
+
+        }
+
+
+        if (Objects.equals(perspective, "BLACK")) {
+            for (int r =7; r >= 0; r--) {
+                for (int c=7; c >= 0; c--) {
+                    System.out.print(board[r][c]);
+                }
+                System.out.println(EscapeSequences.RESET_BG_COLOR);
+            }
+        } else {
+            for (int r =7; r >= 0; r--) {
+                System.out.print((r + 1) + " ");
+                for (int c=0; c < 8; c++) {
+                    System.out.print(board[r][c]);
+                }
+                System.out.println(EscapeSequences.RESET_BG_COLOR);
+            }
+        }
 
     }
 }
