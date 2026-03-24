3c7f4085d17d701b284d9ba9da22dc00b0ba8196
Kiara Chambers <KiaraChambers3@gmail.com>
2026-03-23 20:18:32 -0600

    start main file

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -3,8 +3,20 @@ package client;
 import chess.*;
 
 public class ClientMain {
+    public static ServerFacade facade;
     public static void main(String[] args) {
-        var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
-        System.out.println("♕ 240 Chess Client: " + piece);
+        //var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
+        facade = new ServerFacade(8080);
+        System.out.println("♕ 240 Chess Client");
+
+        //menu
+    }
+
+    public static void menu(){}
+    
+
+
+    public static void drawChessBoard(boolean perspective){
+
     }
 }
