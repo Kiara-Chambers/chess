bab9f8698f48c859df0f4d3c6a08ea9346e80ab4
Kiara Chambers <KiaraChambers3@gmail.com>
2026-03-25 13:09:31 -0600

    fixed stupidity

--- client/src/main/java/client/ClientMain.java
+++ client/src/main/java/client/ClientMain.java
@@ -16,7 +16,7 @@ import java.util.Scanner;
 import static java.lang.System.exit;
 
 //make the pieces swap spots
-//parameters observe
+//parameters amount
 public class ClientMain {
     public static ServerFacade facade;
     public static boolean loggedIn = false;
@@ -46,14 +46,14 @@ public class ClientMain {
                     quit();
                     break;
                 case "login":
-                    if (parts.length !=2) {
+                    if (parts.length !=3) {
                         System.out.println("Try again and use like this: login <username> <password>");
                         menu();
                     }
                     handleLogin(parts[1],parts[2]);
                     break;
                 case "register":
-                    if (parts.length != 3) {
+                    if (parts.length != 4) {
                         System.out.println("Try again and use like this: register <username> <password> <email>");
                         menu();
                     }
@@ -68,7 +68,7 @@ public class ClientMain {
         } else {
             switch (userInput) {
                 case "create":
-                    if (parts.length !=1) {
+                    if (parts.length !=2) {
                         System.out.println("Try again and use like this: create <NAME>");
                         menu();
                     }
@@ -78,14 +78,14 @@ public class ClientMain {
                     handleListGames();
                     break;
                 case "join":
-                    if (parts.length !=2) {
+                    if (parts.length !=3) {
                         System.out.println("Try again and use like this: join <NUMBER> <WHITE|BLACK>");
                         menu();
                     }
                     handleJoinGame(parts[1], parts[2]);
                     break;
                 case "observe":
-                    if (parts.length !=1) {
+                    if (parts.length !=2) {
                         System.out.println("Try again and use like this: observe <NUMBER>");
                         menu();
                     }
